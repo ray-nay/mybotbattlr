63b210187976bb66480655a78e7305b91173a0b7
Renee Mumbi <1723renee@gmail.com>
2023-01-24 16:25:04 +0300

    Create BotCard2 function

---
+++ src/components/BotCard2.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const botTypeClasses = {
+  Assault: "icon military",
+  Defender: "icon shield",
+  Support: "icon plus circle",
+  Medic: "icon ambulance",
+  Witch: "icon magic",
+  Captain: "icon star",
+};
+
+function BotCard2(){
+    
+}
