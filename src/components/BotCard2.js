e66aba4f2a9cbfee3be05370a6de45a606f09756
Renee Mumbi <1723renee@gmail.com>
2023-01-24 16:32:51 +0300

    Pass in handleBotClick to the onClick

--- src/components/BotCard2.js
+++ src/components/BotCard2.js
@@ -14,5 +14,47 @@ function BotCard2({bot, botsArmy, setBotsArmy}){
     function handleBotClick(){
 
         const remainingBotsArmy = botsArmy.filter(botArmy => botArmy.id !== bot.id);
+        setBotsArmy(remainingBotsArmy)
     }
+
+    return (
+        <div className="ui column">
+        <div
+          className="ui card"
+          key={bot.id}
+          onClick={handleBotClick}
+        >
+          <div className="image">
+            <img alt="oh no!" src={bot.avatar_url} />
+          </div>
+          <div className="content">
+            <div className="header">
+              {bot.name}
+              <i className={botTypeClasses[bot.bot_class]} />
+            </div>
+            <div className="meta text-wrap">
+              <small>{bot.catchphrase}</small>
+            </div>
+          </div>
+          <div className="extra content">
+            <span>
+              <i className="icon heartbeat" />
+              {bot.health}
+            </span>
+  
+            <span>
+              <i className="icon lightning" />
+              {bot.damage}
+            </span>
+            <span>
+              <i className="icon shield" />
+              {bot.armor}
+            </span>
+           
+          </div>
+        </div>
+      </div>
+    );
 }
+
+export default BotCard2;
